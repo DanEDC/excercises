36f3dbcf6b99da3242735587315f8d5777f7c482
DanEDC <kursantjava48@gmail.com>
2017-10-16 16:28:29 +0200

    First program from Udemy

---
+++ src/com/daniels/Main.java
@@ -0,0 +1,21 @@
+package com.daniels;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte myByte = -100;
+        byte myTotalByte = (byte) (myByte / 13);
+
+        short myShort = 32000;
+        short myTotalShort = (short) (myShort / 111);
+
+        int myInt = 1331432131;
+        int myTotalInt = ((myInt - 1000222) / 500);
+
+        long myLong = 11111111111111L;
+        long myTotalLong = (long) (50000 + (10 * (myTotalByte + myShort + myTotalInt)));
+
+        System.out.println(myTotalLong);
+    }
+}
