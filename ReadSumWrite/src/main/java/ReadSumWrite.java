4945cb4da13e65e6770157b13a6805eef1ea598b
DanEDC <kursantjava48@gmail.com>
2017-11-02 15:45:30 +0100

    First version of the program

---
+++ ReadSumWrite/src/main/java/ReadSumWrite.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ReadSumWrite {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt" ));
+        while (scanner.hasNextLine()){
+            if (!scanner.hasNextInt()) {
+                scanner.nextLine();
+            } else {
+                String line = scanner.nextLine();
+                Pattern pattern = new Pattern("");
+                Matcher matcher = pattern.matcher(line);
+                matcher.
+                Scanner numbersScanner = new Scanner(line);
+                List<Integer> lineIntegers = new ArrayList<Integer>();
+                while (numbersScanner.hasNextInt()) {
+                    lineIntegers.add(numbersScanner.nextInt());
+                }
+                System.out.println(lineIntegers);
+            }
+        }
+    }
+}
