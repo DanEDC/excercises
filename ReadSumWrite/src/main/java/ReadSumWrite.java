8711ea5e2a2338b56a8e8f0a07d8dfd7609d9e1f
DanEDC <kursantjava48@gmail.com>
2017-11-06 15:46:51 +0100

    upadted version

--- ReadSumWrite/src/main/java/ReadSumWrite.java
+++ ReadSumWrite/src/main/java/ReadSumWrite.java
@@ -8,24 +8,43 @@ import java.util.regex.Pattern;
 
 public class ReadSumWrite {
 
+
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(new File("D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt" ));
-        while (scanner.hasNextLine()){
-            if (!scanner.hasNextInt()) {
-                scanner.nextLine();
-            } else {
-                String line = scanner.nextLine();
-                Pattern pattern = new Pattern("");
-                Matcher matcher = pattern.matcher(line);
-                matcher.
-                Scanner numbersScanner = new Scanner(line);
-                List<Integer> lineIntegers = new ArrayList<Integer>();
-                while (numbersScanner.hasNextInt()) {
-                    lineIntegers.add(numbersScanner.nextInt());
+        {
+            Scanner scanner = new Scanner(new File("D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt"));
+            while (scanner.hasNextLine()) {
+                if (!scanner.hasNextInt()) {
+                    scanner.nextLine();
+                } else {
+                    String line = scanner.nextLine();
+                    Pattern pattern = Pattern.compile("\\s");
+                    Matcher matcher = pattern.matcher(line); // 1 2 5 6 -> OK /// 1 jakiś tekst -> Źle
+                    Scanner numbersScanner = new Scanner(line);
+                    List<Integer> lineIntegers = new ArrayList<Integer>();
+                    while (numbersScanner.hasNextInt()) {
+                        lineIntegers.add(numbersScanner.nextInt());
+                    }
+                    printComponents(lineIntegers);
+                    System.out.println(sum(lineIntegers));
+
                 }
-                System.out.println(lineIntegers);
             }
         }
     }
+
+    private static void printComponents(List<Integer> l) {
+        System.out.println(l);
+    }
+
+    private static Integer sum(List<Integer> l) {
+        return 0;
+    }
+
+
 }
+
+
+
+
+
