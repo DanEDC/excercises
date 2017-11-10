f56df080f3238173d8533ac874451bbbd3ee15cf
DanEDC <kursantjava48@gmail.com>
2017-11-10 17:11:09 +0100

    fixed code errors

--- ReadSumWrite/src/main/java/ReadSumWrite.java
+++ ReadSumWrite/src/main/java/ReadSumWrite.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -12,48 +13,50 @@ public class ReadSumWrite {
     public static void main(String[] args) throws FileNotFoundException {
 
         {
-            Scanner scanner = new Scanner( new File( "D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt" ) );
+            Scanner scanner = new Scanner(new File("D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt"));
             while (scanner.hasNextLine()) {
                 if (!scanner.hasNextInt()) {
                     scanner.nextLine();
                 } else {
                     String line = scanner.nextLine();
-                    Pattern pattern = Pattern.compile( "\\s" );
-                    Matcher matcher = pattern.matcher( line ); // 1 2 5 6 -> OK /// 1 jakiś tekst -> Źle
-                    Scanner numbersScanner = new Scanner( line );
+                    Pattern pattern = Pattern.compile("\\s");
+                    Matcher matcher = pattern.matcher(line); // 1 2 5 6 -> OK /// 1 jakiś tekst -> Źle
+                    Scanner numbersScanner = new Scanner(line);
                     List<Integer> lineIntegers = new ArrayList<Integer>();
                     while (numbersScanner.hasNextInt()) {
-                        lineIntegers.add( numbersScanner.nextInt() );
+                        lineIntegers.add(numbersScanner.nextInt());
                     }
                     if (!lineIntegers.isEmpty()) {
-                        printComponents( lineIntegers );
-
+                        printComponents(lineIntegers);
                     }
                 }
+            }
+            FileWriter writer = new FileWriter("output.txt");
+
             }
         }
     }
 
     private static void printComponents(List<Integer> l) {
-        StringBuilder builder = new StringBuilder( "[" );
-        for (int i : l) builder.append( i ).append( " + " );
-        if (l.size() > 0) builder.deleteCharAt( builder.length() - 1 );
-        if (l.size() > 0) builder.deleteCharAt( builder.length() - 1 );
-        builder.append( "= " ).append( sum( l ) ).append( "]" );
-        builder.toString();
-        System.out.println( builder );
+        StringBuilder builder = new StringBuilder("[");
+        for (int i : l) builder.append(i).append(" + ");
+        if (l.size() > 0) builder.deleteCharAt(builder.length() - 1);
+        if (l.size() > 0) builder.deleteCharAt(builder.length() - 1);
+        builder.append("= ").append(sum(l)).append("]");
+        System.out.println(builder.toString());
     }
 
     private static Integer sum(List<Integer> l) {
         Integer sum = 0;
-        for (int i = 0; i < l.size(); i++)
-            sum += new Integer( l.get( i ) );
-        {
-            return sum;
+        for (int i : l) {
+            sum += i;
         }
-
+        return sum;
     }
+
 }
+
+
 
 
 
