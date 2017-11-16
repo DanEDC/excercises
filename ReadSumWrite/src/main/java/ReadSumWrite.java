563aec73b531b0859ef6d163554a4bb2d3ed9423
DanEDC <kursantjava48@gmail.com>
2017-11-16 14:00:41 +0100

    updated code

--- ReadSumWrite/src/main/java/ReadSumWrite.java
+++ ReadSumWrite/src/main/java/ReadSumWrite.java
@@ -1,49 +1,47 @@
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ReadSumWrite {
 
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         {
             Scanner scanner = new Scanner(new File("D:\\Users\\502123448\\Desktop\\Work\\Git\\solutions-3-daniel-edc\\solutions-3-daniel-edc\\ReadSumWrite\\src\\main\\resources\\1000.txt"));
+            FileWriter writer = new FileWriter("output.txt");
             while (scanner.hasNextLine()) {
                 if (!scanner.hasNextInt()) {
                     scanner.nextLine();
                 } else {
                     String line = scanner.nextLine();
                     Pattern pattern = Pattern.compile("\\s");
-                    Matcher matcher = pattern.matcher(line); // 1 2 5 6 -> OK /// 1 jakiś tekst -> Źle
                     Scanner numbersScanner = new Scanner(line);
                     List<Integer> lineIntegers = new ArrayList<Integer>();
                     while (numbersScanner.hasNextInt()) {
                         lineIntegers.add(numbersScanner.nextInt());
                     }
                     if (!lineIntegers.isEmpty()) {
-                        printComponents(lineIntegers);
+                        printComponents(writer, lineIntegers);
                     }
                 }
             }
-            FileWriter writer = new FileWriter("output.txt");
-
-            }
+            writer.close();
         }
     }
 
-    private static void printComponents(List<Integer> l) {
+
+    private static void printComponents(FileWriter writer, List<Integer> l) throws IOException {
         StringBuilder builder = new StringBuilder("[");
         for (int i : l) builder.append(i).append(" + ");
         if (l.size() > 0) builder.deleteCharAt(builder.length() - 1);
         if (l.size() > 0) builder.deleteCharAt(builder.length() - 1);
-        builder.append("= ").append(sum(l)).append("]");
-        System.out.println(builder.toString());
+        builder.append("= ").append(sum(l)).append("]").append('\n');
+        writer.write(builder.toString());
     }
 
     private static Integer sum(List<Integer> l) {
